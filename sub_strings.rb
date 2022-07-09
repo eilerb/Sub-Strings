5c194104f4b95b0579e5bd546f13b6617be25431
eb <eiler.byberi@gmail.com>
2022-07-08 19:40:49 -0700

    Fix formatting

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,14 +1,10 @@
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
-def substrings(string, dict) 
+def substrings(string, dict)
   string = string.downcase
-  count = string.split(' ').reduce(Hash.new(0)) do |hash, word|
-    dict.each {|element| hash[element] += 1 if word.include?(element)}
-    hash
+  string.split(' ').each_with_object(Hash.new(0)) do |word, hash|
+    dict.each { |element| hash[element] += 1 if word.include?(element) }
   end
-  return count
 end
 
 substrings("Howdy partner, sit down! How's it going?", dictionary)
-
